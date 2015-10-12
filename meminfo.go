291d32907e65d80b1ea6a301be640811952c7def
Martin Garton <martin.garton@ft.com>
2015-10-12 21:29:56 +0100

    give memory checker a type

--- meminfo.go
+++ meminfo.go
@@ -6,7 +6,26 @@ import (
 	linuxproc "github.com/c9s/goprocinfo/linux"
 )
 
-func avMemoryCheck() error {
+type memoryChecker struct {
+	thresholdPercent float64
+}
+
+func (mc memoryChecker) Checks() []fthealth.Check {
+
+	check := fthealth.Check{
+		BusinessImpact:   "A part of the publishing workflow might be affected",
+		Name:             "Memory load check",
+		PanicGuide:       "Please refer to technical summary",
+		Severity:         2,
+		TechnicalSummary: "Check the memory usage of services/containers on this host, please proceed conform these values.",
+		Checker:          mc.avMemoryCheck,
+	}
+
+	return []fthealth.Check{check}
+}
+
+func (mc memoryChecker) avMemoryCheck() error {
+
 	meminfo, err := linuxproc.ReadMemInfo(*hostPath + "/proc/meminfo")
 	if err != nil {
 		return err
@@ -14,20 +33,8 @@ func avMemoryCheck() error {
 	available := meminfo.MemAvailable
 	total := meminfo.MemTotal
 	availablePercent := float64(available) / float64(total) * 100
-	if availablePercent < 20 {
+	if availablePercent < mc.thresholdPercent {
 		return fmt.Errorf("Low available memory: %2.1f %%", availablePercent)
 	}
 	return nil
-}
-
-func MemInfo(checks *[]fthealth.Check) {
-	memAvCheck := fthealth.Check{
-		BusinessImpact:   "A part of the publishing workflow might be affected",
-		Name:             "Memory load check",
-		PanicGuide:       "Please refer to technical summary",
-		Severity:         2,
-		TechnicalSummary: "Check the memory usage of services/containers on this host, please proceed conform these values.",
-		Checker:          avMemoryCheck,
-	}
-	*checks = append(*checks, memAvCheck)
 }
--- register_checks.go
+++ register_checks.go
@@ -6,7 +6,7 @@ import (
 
 func RegisterChecks(checks *[]fthealth.Check) {
 	DiskFreeChecks(checks)
-	MemInfo(checks)
+	*checks = append(*checks, memoryChecker{20}.Checks()...)
 	*checks = append(*checks, loadAverageChecker{}.Checks()...)
 	*checks = append(*checks, inodeChecker{1024}.Checks()...)
 	*checks = append(*checks, contextSwitchChecker{120000}.Checks()...)
