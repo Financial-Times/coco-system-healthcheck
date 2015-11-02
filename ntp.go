7b4c58f9366501d8b078bca1182f95002fd4a170
George Angel <george.angel.87@gmail.com>
2015-11-02 23:17:54 +0000

    NTP check - try 2
    Now runs ntpd inside the container (should be there) but docker uses
    system time by default. Currently just checks if offset is greater then
    100 seconds

--- ntp.go
+++ ntp.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/Financial-Times/go-fthealth"
 	"os/exec"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type ntpChecker struct{}
 
+type offsetResult struct {
+	val float64
+	err error
+}
+
+var offsetCh chan offsetResult
+
 func (ntpc ntpChecker) Checks() []fthealth.Check {
+	offsetCh = make(chan offsetResult)
+	go ntpLoop()
+
 	ntpCheck := fthealth.Check{
 		BusinessImpact:   "A part of the publishing workflow might be affected",
 		Name:             "NTP sync check",
@@ -22,22 +35,57 @@ func (ntpc ntpChecker) Checks() []fthealth.Check {
 }
 
 func (ntpc ntpChecker) Check() error {
-	var (
-		cmdOut []byte
-		err    error
-	)
-	if cmdOut, err = exec.Command("/usr/bin/timedatectl").Output(); err != nil {
-		return fmt.Errorf("Could not run `timedatectl`")
+	offset := <-offsetCh
+	if offset.err != nil {
+		return offset.err
 	}
-	out := string(cmdOut)
-	anchor := "NTP synchronized: "
-	index := strings.Index(out, anchor)
-	if index == -1 {
-		return fmt.Errorf("Failed to get `NTP synchronized` from `timedatectl` command")
-	}
-	answer := out[index+len(anchor) : index+len(anchor)+3]
-	if answer != "yes" {
-		return fmt.Errorf("NTP is not synchronized")
+	if offset.val > 100 || offset.val < -100 {
+		return fmt.Errorf("offset is greater then limit of 100: %f", offset.val)
 	}
 	return nil
+}
+
+func ntpLoop() {
+	update := make(chan offsetResult)
+	go func() {
+		for {
+			update <- ntpOffset(ntpCmd)
+			time.Sleep(10 * time.Minute)
+		}
+	}()
+
+	offset := offsetResult{err: errors.New("Ntp offset not initialised")}
+	for {
+		select {
+		case offsetCh <- offset:
+		case o := <-update:
+			offset = o
+		}
+	}
+}
+
+func ntpCmd() string {
+	// Throw away the error, it never returns 0, and we can catch an actual error later
+	cmdOut, _ := exec.Command("ntpd", "-q", "-n", "-w", "-p", "pool.ntp.org").CombinedOutput()
+	return string(cmdOut)
+}
+
+func ntpOffset(ntpCmd func() string) offsetResult {
+	var offset string
+	out := strings.Split(ntpCmd(), " ")
+	for _, str := range out {
+		if strings.Index(str, "offset") != -1 {
+			offset = strings.Split(str, ":")[1]
+			break
+		}
+	}
+	if offset == "" {
+		return offsetResult{0, fmt.Errorf("ntpd did not return an offset value")}
+	}
+	offsetFloat, err := strconv.ParseFloat(offset, 64)
+	if err != nil {
+		return offsetResult{0, fmt.Errorf("Could not parse offset: %v", err)}
+	}
+
+	return offsetResult{offsetFloat, nil}
 }
