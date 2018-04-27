a5ec211355c331c28d1b2776c953859d7886bf96
George Antoniadis <george@noodles.gr>
2018-04-27 08:46:35 +0100

    Use new pools on tests

--- ntp_test.go
+++ ntp_test.go
@@ -7,9 +7,25 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var reachablePools = []string{"0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org", "3.pool.ntp.org"}
-var someReachablePools = []string{"foobar8237.ntp.org", "1.pool.ntp.org", "foobarlllx237.ntp.org", "3.pool.ntp.org"}
-var unreachablePools = []string{"foobar8237.ntp.org", "foobarlllx237.ntp.org"}
+var reachablePools = []string{
+	"0.coreos.pool.ntp.org",
+	"1.coreos.pool.ntp.org",
+	"2.coreos.pool.ntp.org",
+	"3.coreos.pool.ntp.org",
+	"0.pool.ntp.org",
+	"2.pool.ntp.org",
+}
+var someReachablePools = []string{
+	"foobar8237.ntp.org",
+	"0.coreos.pool.ntp.org",
+	"1.pool.ntp.org",
+	"foobarlllx237.ntp.org",
+	"3.pool.ntp.org",
+}
+var unreachablePools = []string{
+	"foobar8237.ntp.org",
+	"foobarlllx237.ntp.org",
+}
 
 func TestCallNtpWithPoolsAllPoolsReachable(t *testing.T) {
 	ntpc := &ntpCheckerImpl{
